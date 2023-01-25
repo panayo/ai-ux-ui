fe9bf2785e8a627efc69c6c00dca9397b9ed4cab
panayo <papoutsispanayotis@gmail.com>
2023-01-25 11:32:09 +0200

    Test the get_driver function

--- app/frontend.py
+++ app/frontend.py
@@ -132,7 +132,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
+@st.experimental_memo(show_spinner=False,suppress_st_warning=True)
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
